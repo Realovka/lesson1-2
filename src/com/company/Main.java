dc85cc8f548f2fdf149fc6fda2e68a95bcffe305
Nastya <realovka@mail.ru>
2020-04-27 19:10:21 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+// приведение типов
+public class Main {
+
+    public static void main(String[] args) {
+
+        long ch;
+        long a;
+
+        double b = 201236548974563215.12894;
+        ch=(long) b;
+        System.out.println(ch);
+
+        float f=  201236548974563215.12894f;
+        a=(long) b;
+        System.out.println(a);
+
+
+
+    }
+}
