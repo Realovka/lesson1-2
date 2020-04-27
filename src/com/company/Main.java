565b9c11eda7734ab722a514ca738025ebadcccd
Nastya <realovka@mail.ru>
2020-04-27 20:49:20 +0300

    rtrtyr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 // приведение типов
 public class Main {
-
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         long ch;
         long a;
 
-        double b = 201236548974563215.12894;
+        double b = 2012365489745632.12894;
         ch=(long) b;
         System.out.println(ch);
 
